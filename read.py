550bcab4417083193ab7688965c365202d5c1803
Yuting Sun <doll72141@gmail.com>
2020-11-04 19:41:21 +0800

    add filetering function

--- read.py
+++ read.py
@@ -10,6 +10,14 @@ print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 sum_len = 0
 for d in data:
-	sum_len = sum_len + len(d)
+    sum_len += len(d)
 
 print('留言的平均長度為', sum_len / len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
